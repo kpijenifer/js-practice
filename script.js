b3e0b3a86989f5ad175e7a116a436606c7e3ee08
kpijenifer <jenifermech7@gmail.com>
2022-08-05 16:44:24 +0530

    add-file.

---
+++ script.js
@@ -0,0 +1,20 @@
+// //addition//
+// let a=10, b=20,c;
+//  c=a+b;
+//  console.log(c);
+
+ let arr  =[1,2,3,4,5]
+let cb =[7,8,64]
+ let ab  = arr.pop(cb)
+ console.log(ab);
+
+let array =[1000,2000,3000,4000,5000,6000,7000]
+ let ck =[8000,9000,500]
+array.unshift('uybhn');
+console.log(array);
+ck.unshift('jefj..wgbf  .lbwef.ljh');
+console.log(ck)
+let jeni=array.concat(ck);
+console.log(jeni);  
+
+
