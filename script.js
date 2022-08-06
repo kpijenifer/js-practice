56c06771f57983b0f3832dd263cac6fe9adaf112
kpijenifer <jenifermech7@gmail.com>
2022-08-06 12:45:20 +0530

    add-file.

--- script.js
+++ script.js
@@ -3,18 +3,60 @@
 //  c=a+b;
 //  console.log(c);
 
- let arr  =[1,2,3,4,5]
-let cb =[7,8,64]
- let ab  = arr.pop(cb)
- console.log(ab);
+//  let arr  =[1,2,3,4,5]
+// let cb =[7,8,64]
+//  let ab  = arr.pop(cb)
+//  console.log(ab);
 
-let array =[1000,2000,3000,4000,5000,6000,7000]
- let ck =[8000,9000,500]
-array.unshift('uybhn');
-console.log(array);
-ck.unshift('jefj..wgbf  .lbwef.ljh');
-console.log(ck)
-let jeni=array.concat(ck);
-console.log(jeni);  
+// let array =[1000,2000,3000,4000,5000,6000,7000]
+//  let ck =[8000,9000,500]
+// array.unshift('uybhn');
+// console.log(array);
+// ck.unshift('jefj..wgbf  .lbwef.ljh');
+// console.log(ck)
+// let jeni=array.concat(ck);
+// console.log(jeni);
+// addition
+// let a,b,c;
+//   a=14,b=17;
+//   c=a+b;
+//   console.log(c);
+//   multification
+//  c=a*b;
+//  console.log(c);
+//  subtraction
+//  c=a-b;
+//  console.log(c);
+//  c=a/b;
+//  console.log(c);
+// area of triangle
+
+//  a=3.14r**2
+// let d=3.14;
+// r=parseFloat(prompt("enter the value r"));
+// a=d*r**2;
+// console.log(a);
+//area of triangle
+// m=1/2*b*h;
+
+let b,h;
+let k=1/2;
+b=parseFloat(prompt("enter the value b"));
+h=parseFloat(prompt("enter the value h"));
+m=k*b*h;
+console.log(m);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
