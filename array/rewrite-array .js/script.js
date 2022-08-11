31b649c00c8bdcd55f976373193e97b6f9a8fe50
kpijenifer <jenifermech7@gmail.com>
2022-08-11 16:01:49 +0530

    add-file.

---
+++ array/rewrite-array .js/script.js
@@ -0,0 +1,67 @@
+// var a=[10,20,30,40,50,60];
+// // a.push(15,70);
+// // console.log(a);
+// function number(array,value){
+//     length=array.length
+//     array[length]=value
+//     return array;
+
+// }
+// a=number(a,30);
+// console.log(a);
+
+
+
+var animals=["lion","cat","dog","rat","elephant"];
+// animals.push("hen");
+// console.log(animals);
+
+function mypush(array,value){
+    var length=animals.length;
+
+    
+    animals[length]=value;
+    return animals;
+
+
+}
+animals=mypush(animals,"peak");
+console.log(animals);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- array/script.js
+++ array/script.js
@@ -32,87 +32,150 @@
 
 //     }
 //     else{
-//         console.log(c);
-//     }
-// }
-// let c=30;
-// for(i=0;i<=c;i++){
-//     console.log(i);
-// }
+// //         console.log(c);
+// //     }
+// // }
+// // let c=30;
+// // for(i=0;i<=c;i++){
+// //     console.log(i);
+// // }
 
-// prototype
-// push
-   let  arrayname=["a","b","c"];
-arrayname.push("d");
-console.log(arrayname);
+// // prototype
+// // push
+//    let  arrayname=["a","b","c"];
+// arrayname.push("d");
+// console.log(arrayname);
 
-// pop
-let department=["mechanical","electrical","civil","truply"];
-// department.pop();
-console.log(department.pop());
+// // pop
+// let department=["mechanical","electrical","civil","truply"];
+// // department.pop();
+// console.log(department.pop());
 
-// shift
-   let subject=["tamil","english","maths","science","physics"];
-subject.shift();
-console.log(subject);
-
-// unshift
-let animials=["lion","cat","rat","elephant"];
-animials.unshift("dog");
-console.log(animials);
-
-// slice
-
-let india=["arun","jeni","mani","calep","kalep"];
- mish=india.slice(2,3);
-console.log(mish);
-mish1=india.slice(-3,-1);
-console.log(mish1);
-mish2=india.slice(-4);
-console.log(mish2);
-mish3=india.slice(2);
-console.log(mish3);
-mish4=india.slice(1,4);
-console.log(mish4);
+// // shift
+//    let subject=["tamil","english","maths","science","physics"];
+// subject.shift();
+// console.log(subject);
 
 
+// // unshift
+// let animials=["lion","cat","rat","elephant"];
+// animials.unshift("dog");
+// console.log(animials);
 
-// sort
+// // slice
 
-let marks=[10,30,50,15,20,34,25,17];
-marks.sort();
-console.log(marks);
+// let india=["arun","jeni","mani","calep","kalep"];
+//  mish=india.slice(2,3);
+// console.log(mish);
+// mish1=india.slice(-3,-1);
+// console.log(mish1);
+// mish2=india.slice(-4);
+// console.log(mish2);
+// mish3=india.slice(2);
+// console.log(mish3);
+// mish4=india.slice(1,4);
+// console.log(mish4);
 
 
 
-// splice
-let birds=["peacock","crow","eagle","dove","hen"];
-birds.splice(2,2,"parrot");
-console.log(birds);
+// // sort
 
-// tostring
-let a=true;
-// var b=a.toString();
-
-console.log( typeof a);
-
-// index.of"
-let u=["ab","cd","ef","gh","ij","kl","mn","op"];
-u.indexOf("gh");
-console.log(u.indexOf("gh"));
-
-
-let q=["army","police","cisf","crpf","navy","airforce"]
-console.log(q.indexOf("airforce"));
+// let marks=[10,30,50,15,20,34,25,17];
+// marks.sort();
+// console.log(marks);
 
 
 
+// // splice
+// let birds=["peacock","crow","eagle","dove","hen"];
+// birds.splice(2,2,"parrot");
+// console.log(birds);
+
+// // tostring
+// let a=true;
+// // var b=a.toString();
+
+// console.log( typeof a);
+
+// // index.of"
+// let u=["ab","cd","ef","gh","ij","kl","mn","op"];
+// u.indexOf("gh");
+// console.log(u.indexOf("gh"));
+
+
+// let q=["army","police","cisf","crpf","navy","airforce"]
+// console.log(q.indexOf("airforce"));
 
 
 
 
+// simple interest\
+// S.I=p*t*r/100;
+// let p=20;
+// let t=30;
+// let r=40;
+// let p,t,r;
+// p=parseInt(prompt("enter the value p"));
+// t=parseInt(prompt("enter the value t"));
+// r=parseInt(prompt("enter the value r"));
+// s=p*t*r/100;
+// console.log(s);
+
+
+// squre
+
+// a=s*s;
+
+// let s;
+// s=parseInt(prompt("enter the value s"));
+// a=s*s;
+// console.log(a);
+
+// cube
+// s=a**3;
+// a=parseInt(prompt("enter the value a"));
+// s=a**3;
+// console.log(s);
+
+
+// swap
+
+
+// let a=15;
+// let b=88;
+// let c;
+// c=a;
+// a=b;
+// a=c;
+// console.log(a);
+// console.log(b);
+
+// total and average
+
+// let t=85;
+// let e=69;
+// let m=77;
+// let s=97;
+// let ss=98;
+// let total;
+
+// total=t+e+m+s+ss;
+// console.log(total);
+
+// large number
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+   
